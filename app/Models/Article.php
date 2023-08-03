7f0d6fefe2c514b41eb85c1c6a36c4a76d256cc6
James Timms <jtimms1998@gmail.com>
2023-08-03 20:22:30 +0000

    Edited the Models to have relationships

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class);
+    }
 }
