aff991b49e68b70324db3b81941f8ff7074e19d1
James Timms <jtimms1998@gmail.com>
2023-08-03 19:40:29 +0000

    Set up Auth Scaffolding and Middleware for Bootstrap + Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        /// Roles
+    Role::create(['name' => 'admin']);
+    Role::create(['name' => 'editor']);
+    Role::create(['name' => 'member']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    // Permissions (e.g., 'create-article', 'edit-article', etc.)
+    Permission::create(['name' => 'create-article']);
+    Permission::create(['name' => 'edit-article']);
+    Permission::create(['name' => 'delete-article']);
+
+    // Assign permissions to roles
+    $admin = Role::findByName('admin');
+    $admin->givePermissionTo(['create-article', 'edit-article', 'delete-article']);
+
+    $editor = Role::findByName('editor');
+    $editor->givePermissionTo(['create-article', 'edit-article']);
+
+    // Assign roles to users (you can do this in the registration or in seeds)
+    $user = App\Models\User::find(1); // Replace 1 with the ID of an existing user
+    $user->assignRole('admin');
     }
 }
